2f9edeb5a8229096b6a3ab4abf6115ca66efe2ce
Bella Amandine <bellamandy45@gmail.com>
2021-02-11 20:23:59 +0200

    Add new vehicle form, add post vehicle view function and new vehicle template

--- carapp/urls.py
+++ carapp/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name = 'register'),
+    path('index/', views.index, name = 'index'),
+    path('post/vehicle/', views.post_vehicle, name = 'new_vehicle'),
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- carapp/views.py
+++ carapp/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .forms import SignupForm
+from .forms import SignupForm, PostVehicleForm
+from .models import Profile, Vehicle, RequestRent
 
 # Create your views here.
 def signup(request):
@@ -8,8 +9,27 @@ def signup(request):
 
         if form.is_valid():
             new_user = form.save()
-            
+
+            new_profile = Profile(user = new_user)
+            new_profile.create_profile()
+
             return redirect('login')
     else:
         form = SignupForm()
         return render(request, 'registration/signup.html', {'form': form})
+
+def index(request):
+    return render(request, 'car/index.html')
+
+def post_vehicle(request):
+    if request.method == 'POST':
+        form = PostVehicleForm(request.POST, request.FILES)
+        if form.is_valid():
+            new_vehicle = form.save(commit=False)
+            new_vehicle.owner = request.user.profile
+            new_vehicle.save()
+
+            return redirect('index')
+    else:
+        form = PostVehicleForm()
+        return render(request, 'car/new-vehicle.html', {'form': form})
