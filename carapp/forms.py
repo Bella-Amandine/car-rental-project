b0ef0ba9a12f64079f5f6d0d38d1d00fc793eca0
Bella Amandine <bellamandy45@gmail.com>
2021-02-11 10:15:48 +0200

    Migrate Profile, Vehicle, RequestRent

--- carapp/forms.py
+++ carapp/forms.py
@@ -3,7 +3,6 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
 class SignupForm(UserCreationForm):
-    print(User)
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
