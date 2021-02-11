42feb46af90c414b5f1f66768f3940bf17507955
Bella Amandine <bellamandy45@gmail.com>
2021-02-11 21:26:04 +0200

    Add single vehicle urls and index urls

--- carapp/urls.py
+++ carapp/urls.py
@@ -5,8 +5,9 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.signup, name = 'register'),
-    path('index/', views.index, name = 'index'),
+    path('', views.index, name = 'index'),
     path('post/vehicle/', views.post_vehicle, name = 'new_vehicle'),
+    path('vehicle/<vehicle_id>', views.single_vehicle, name = 'vehicle')
 ]
 if settings.DEBUG:
     urlpatterns+=static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
